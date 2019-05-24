672fdb7032122c7d9f327e4851e45b7c76679e16
Vinay Kothapally <vinaykothapally@Vinay.local>
2019-05-24 03:01:19 -0500

    Added Microphone Arrays

--- simReverb.py
+++ simReverb.py
@@ -7,7 +7,8 @@ Created on Wed May  8 12:32:34 2019
 """
 
 import os
-import gpuRIR, itertools
+#import gpuRIR
+import itertools
 import numpy as np
 import pandas as pd
 import numpy.matlib
@@ -19,13 +20,13 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 class simRIR:
     
-    def __init__(self, nrooms, nmics, maxdistance, nT60s):
+    def __init__(self, nrooms, narrays, maxdistance, nT60s):
         self.rooms = self.build(nrooms)
         self.fs = 16000
         self.source()
         self.reverbtime(nT60s)
-        self.microphones(nmics, maxdistance)
-        self.getRIRs()
+        self.microphones(narrays, maxdistance)
+        self.getRIRs_test()
         
          
     def build(self, nrooms):
@@ -34,7 +35,6 @@ class simRIR:
         height = np.round([2.5,4.0,6.0])
         rooms = {}
         rooms_options = list(set(list(itertools.product(length, width, height))))
-        print(len(rooms_options))
         rooms_options = sorted(rooms_options, key=lambda tup: tup[0])
         rooms_options = [rooms_options[int(k)] for k in np.linspace(1,len(rooms_options)-1, nrooms)]
         for k in range(len(rooms_options)):
@@ -55,69 +55,78 @@ class simRIR:
         self.rooms['Source'] = source_location
         print('*** Done Addding Source Location')
         
-    def microphones(self, nmics, maxdistance):
+    def microphones(self, narrays, maxdistance):
         self.rooms['Mic Pattern'] = len(self.rooms)*['omni']
-        micD= np.round(maxdistance*(1-np.exp(-1*np.geomspace(0.03, maxdistance, nmics+2))),decimals=3)
-        micD = micD[1:-1]
+        micD= np.round(maxdistance*(1-np.exp(-1*np.geomspace(0.5, maxdistance, narrays+2))),decimals=3)
+        micD = [1,3,6]
+        self.rooms['nArrays'] = ""
         self.rooms['nMics'] = ""
-        self.rooms['Microphone Location'] = ""
-        self.rooms['Mic Distance'] = ""
+        self.rooms['Array Location'] = ""
+        self.rooms['Array Distance'] = ""
         self.rooms['T60'] = ""
         for i, row in self.rooms.iterrows():
-            self.rooms['nMics'][i] =  int(np.sum(1.0*(micD <= self.rooms['MaxDistance'][i])))
-            self.rooms['Mic Distance'][i] = list(micD[0:self.rooms['nMics'][i]])
-            self.rooms['Microphone Location'][i] = {} 
+            self.rooms['nArrays'][i] =  int(np.sum(1.0*(micD <= self.rooms['MaxDistance'][i])))
+            self.rooms['nMics'][i] =  5*int(np.sum(1.0*(micD <= self.rooms['MaxDistance'][i])))
+            self.rooms['Array Distance'][i] = list(micD[0:self.rooms['nArrays'][i]])
+            self.rooms['Array Location'][i] = {} 
             self.rooms['T60'][i] = self.T60s
-            for k in range(self.rooms['nMics'][i]):
+            for k in range(self.rooms['nArrays'][i]):
                 angle = np.arctan((self.rooms['Dimensions'][i][1]-1)/(self.rooms['Dimensions'][i][0]-1))
-                self.rooms['Microphone Location'][i]['Mic-'+str(k)] = [np.round(self.rooms['Source'][i][0]+micD[k]*np.cos(angle),decimals=2), 
+                array_center = [np.round(self.rooms['Source'][i][0]+micD[k]*np.cos(angle),decimals=2), 
                                                                        np.round(self.rooms['Source'][i][1]+micD[k]*np.sin(angle),decimals=2), 
                                                                        self.rooms['Source'][i][2]]
-                if any(np.array(self.rooms['Microphone Location'][i]['Mic-'+str(k)]) >= np.asarray(self.rooms['Dimensions'][i])):
-                    print('Microphone outside room! Check Logic--- Room: '+str(i)+'  Mic-'+str(k))
+                self.rooms['Array Location'][i]['Array_'+str(k)] = self.array_points(array_center, micD[k], angle)
+                    #if any(np.array(self.rooms['Array Location'][i]['Array-'+str(k)]['Mic-'+str(m)]) >= np.asarray(self.rooms['Dimensions'][i])):
+                    #    print('Microphone outside room! Check Logic--- Room: '+str(i)+'--  Array-'+str(k)+ '--  Mic-'+str(m))
             
         print('*** Done Adding Mictrophones Locations')
         
+    def array_points(self, center, d, theta):
+        array = {}; a = 2e-2;
+        array['Mic_0'] = list(np.round(np.array(center) + np.array([d*np.cos(theta)+a*np.sin(theta),d*np.sin(theta)-a*np.cos(theta),0]), decimals=2))
+        array['Mic_1'] = list(np.round(np.array(center) + np.array([d*np.cos(theta),d*np.sin(theta),0]), decimals=2))
+        array['Mic_2'] = list(np.round(np.array(center) + np.array([d*np.cos(theta)-a*np.sin(theta),d*np.sin(theta)+a*np.cos(theta),0]), decimals=2))
+        array['Mic_3'] = list(np.round(np.array(center) + np.array([(d-a)*np.cos(theta),(d-a)*np.sin(theta),0]), decimals=2))
+        array['Mic_4'] = list(np.round(np.array(center) + np.array([(d+a)*np.cos(theta),(d+a)*np.sin(theta),0]), decimals=2))
+        return array
+
         
     def reverbtime(self, nT60s):
         self.T60s = list(np.round(np.geomspace(100e-3, 10, nT60s), decimals=3))
         print('*** Done Adding T60 Information!')
     
-
-
-    def getRIRs(self):
+    def getRIRs_test(self):
         print('Starting Simulations')
-        self.rooms['Impulse Responses'] = ""
+        self.rooms['RIR'] = ""
         for i, row in tqdm(self.rooms.iterrows(), desc='*** Simulating RIRs'):
-            self.rooms['Impulse Responses'][i] = {}
+            self.rooms['RIR'][i] = {}
             room_sz = list(self.rooms['Dimensions'][i])
             pos_src =  np.array([self.rooms['Source'][i]])
-            for mic in self.rooms['Microphone Location'][i]:
-                self.rooms['Impulse Responses'][i][mic] = {}
-                pos_rcv = np.array([self.rooms['Microphone Location'][i][mic]])
-                for k in range(len(self.T60s)):
-                    self.rooms['Impulse Responses'][i][mic][k] = {}
-                    self.rooms['Impulse Responses'][i][mic][k]['T60'] = self.T60s[k]
-                    beta = gpuRIR.beta_SabineEstimation(room_sz, self.T60s[k], abs_weights=[0.9]*5+[0.5])
-                    Tdiff= gpuRIR.att2t_SabineEstimator(15, self.T60s[k])
-                    nb_img = gpuRIR.t2n(Tdiff, room_sz)
-                    h =  gpuRIR.simulateRIR(room_sz, beta, pos_src, pos_rcv, 
-                                            nb_img, 10, 
-                                            self.fs, 
-                                            Tdiff=Tdiff, 
-                                            orV_rcv=None, 
-                                            mic_pattern=self.rooms['Mic Pattern'][i]).flatten()
-                    self.rooms['Impulse Responses'][i][mic][k]['T60']  = 'Room'+str(i)+'_'+mic+'_T'+str(np.round(self.T60s[k], decimals=2))+'.wav'
-                    sf.write(os.getcwd()+'/RIR_Database/Simulated/'+self.rooms['Impulse Responses'][i][mic][k]['T60'],h,self.fs)
-                    
-        numpy.save(os.getcwd()+'/RIR_Database/Simulated/RoomInfo', self.rooms, allow_pickle=True, fix_imports=True)
+            for k in range(len(self.T60s)):
+                self.rooms['RIR'][i][k] = {}
+                self.rooms['RIR'][i][k]['T60'] = self.T60s[k]
+                for array in self.rooms['Array Location'][i]:
+                    self.rooms['RIR'][i][k][array] = {}
+                    for mic in self.rooms['Array Location'][i][array]:
+                        pos_rcv = np.array([self.rooms['Array Location'][i][array][mic]])
+                        self.rooms['RIR'][i][k][array][mic] = 'Room_'+str(i)+'_RT_'+str(k)+'_'+array+'_'+mic+'.wav'
+                        beta = gpuRIR.beta_SabineEstimation(room_sz, self.T60s[k], abs_weights=[0.9]*5+[0.5])
+                        Tdiff= gpuRIR.att2t_SabineEstimator(15, self.T60s[k])
+                        nb_img = gpuRIR.t2n(Tdiff, room_sz)
+                        h =  gpuRIR.simulateRIR(room_sz, beta, pos_src, pos_rcv, 
+                                                nb_img, 10, 
+                                                self.fs, 
+                                                Tdiff=Tdiff, 
+                                                orV_rcv=None, 
+                                                mic_pattern=self.rooms['Mic Pattern'][i]).flatten()
+                        sf.write(os.getcwd()+'/RIR_Database/Simulated/'+self.rooms['RIR'][i][k][array][mic],h,self.fs)
         print('*** Done Simulating RIRs')
+        numpy.save(os.getcwd()+'/RIR_Database/Simulated/RoomInfo', self.rooms, allow_pickle=True, fix_imports=True)
+        return
+                 
 
-
-
-
-
-Rooms = simRIR(nrooms=200, nmics=5, maxdistance=8, nT60s=12)
+Rooms = simRIR(nrooms=30, narrays=5, maxdistance=8, nT60s=6)
 RoomDatabase = Rooms.rooms
+
 
 
